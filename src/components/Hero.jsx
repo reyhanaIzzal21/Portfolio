8bcf158717707cdafc1f64009f6643fde690c428
Reyhana Izzal <izzalm.008@gmail.com>
2024-12-14 18:06:29 +0700

    update hero 4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-import { EarthMobileCanvas } from "./canvas";
+// import { ComputersCanvas } from "./canvas";
+// import { EarthMobileCanvas } from "./canvas";
 
 
 const Hero = () => {
@@ -45,9 +45,9 @@ const Hero = () => {
       </div>
 
       {/* <ComputersCanvas /> */}
-      <EarthMobileCanvas />
+      {/* <EarthMobileCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
@@ -63,7 +63,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
