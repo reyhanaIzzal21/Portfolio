26e0ff3e0e9d51da2b97a30d53e3c33f7fb9fef1
Reyhana Izzal <izzalm.008@gmail.com>
2024-12-14 16:18:29 +0700

    update hero bg

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <div className='bg-hero'>
+          {/* <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'> */}
           <Navbar />
           <Hero />
         </div>
