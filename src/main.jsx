af4875370e281e12c7cdee371fdb95fcf25787a4
Reyhana Izzal <izzalm.008@gmail.com>
2025-03-28 04:00:44 +0800

    Added SplashCursor component to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import SplashCursor from "./SplashCursor";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <SplashCursor />
   </React.StrictMode>
 );
