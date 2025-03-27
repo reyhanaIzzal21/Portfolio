3bd0f7f9579d309d57c34d9243d8f60c46a8cfc8
Reyhana Izzal <izzalm.008@gmail.com>
2025-03-28 02:53:07 +0700

    Refactor tech used component to use LetterGlitch and update styles

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { motion } from "framer-motion";
+import { LetterGlitch } from "./canvas";
 
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
@@ -11,28 +12,37 @@ const ToolCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='w-full relative right-0 left-0'
+    className="w-full relative"
   >
-    <div className='mt-1 [transform:rotate(-3deg)] overflow-hidden'>
-      <p id="text_1"
-        className='custom-text-outline text-end relative [transform:translateX(-1000px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap'>
-        REACT JS.<span className="span-1 text-pink-600">REACT JS</span>.REACT JS.<span className="span-1 text-pink-600">REACT JS</span>.REACT JS.<span className="span-1 text-pink-600">REACT JS</span>.
-      </p>
-      <p id="text_2"
-        className='custom-text-outline text-start relative [transform:translateX(0px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap'>
-        TAILWIND.<span className="span-2 text-blue-600">TAILWIND</span>.TAILWIND.<span className="span-2 text-blue-600">TAILWIND</span>.TAILWIND.<span className="span-2 text-blue-600">TAILWIND</span>.
-      </p>
-      <p id="text_1"
-        className='custom-text-outline text-end relative [transform:translateX(-1000px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap'>
-        GSAP.<span className="span-3 text-green-600">GSAP</span>.GSAP.<span className="span-3 text-green-600">GSAP</span>.GSAP.<span className="span-3 text-green-600">GSAP</span>.GSAP.<span className="span-3 text-green-600">GSAP</span>
-      </p>
-      <p id="text_2"
-        className='custom-text-outline text-start relative [transform:translateX(0px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap'>
-        THREE JS.<span className="span-4 text-white">THREE JS</span>.THREE JS.<span className="span-4 text-white">THREE JS</span>.THREE JS.<span className="span-4 text-white">THREE JS</span>.
-      </p>
-
+    <div className="mt-1 [transform:rotate(-3deg)] magic-border-wrapper">
+      <div className="magic-border-content relative rounded-3xl overflow-hidden bg-primary h-[18rem] md:h-[30rem]">
+        <div className="absolute inset-0 z-10 h-fit">
+          <p id="text_1"
+            className="custom-text-outline text-end relative [transform:translateX(-1000px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap">
+            REACT JS.<span className="span-1 text-pink-600">REACT JS</span>.REACT JS.<span className="span-1 text-pink-600">REACT JS</span>.REACT JS.<span className="span-1 text-pink-600">REACT JS</span>.
+          </p>
+          <p id="text_2"
+            className="custom-text-outline text-start relative [transform:translateX(0px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap">
+            TAILWIND.<span className="span-2 text-blue-600">TAILWIND</span>.TAILWIND.<span className="span-2 text-blue-600">TAILWIND</span>.TAILWIND.<span className="span-2 text-blue-600">TAILWIND</span>.
+          </p>
+          <p id="text_1"
+            className="custom-text-outline text-end relative [transform:translateX(-1000px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap">
+            GSAP.<span className="span-3 text-green-600">GSAP</span>.GSAP.<span className="span-3 text-green-600">GSAP</span>.GSAP.<span className="span-3 text-green-600">GSAP</span>.GSAP.<span className="span-3 text-green-600">GSAP</span>
+          </p>
+          <p id="text_2"
+            className="custom-text-outline text-start relative [transform:translateX(0px)] text-transparent tracking-wider text-[3rem] md:text-[5rem] uppercase font-extrabold whitespace-nowrap">
+            THREE JS.<span className="span-4 text-white">THREE JS</span>.THREE JS.<span className="span-4 text-white">THREE JS</span>.THREE JS.<span className="span-4 text-white">THREE JS</span>.
+          </p>
+        </div>
+        <LetterGlitch
+          glitchSpeed={50}
+          centerVignette={false}
+          outerVignette={false}
+          smooth={true}
+        />
+      </div>
     </div>
-  </motion.div>
+  </motion.div >
 );
 
 const Feedbacks = () => {
--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,10 +2,12 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
+import LetterGlitch from "./LetterGlitch";
 
 export {
   EarthCanvas,
   BallCanvas,
   ComputersCanvas,
   StarsCanvas,
+  LetterGlitch,
 };
